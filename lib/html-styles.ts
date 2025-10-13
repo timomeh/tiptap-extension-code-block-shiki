69943c00f6a59b185eae4421c5f453c53b499323
Timo Mämecke <timo@maemecke.com>
2025-10-14 01:02:25 +0200

    Remove unnecessary fallback

--- lib/html-styles.ts
+++ lib/html-styles.ts
@@ -1,5 +1,5 @@
 export function styleToHtml(styles: Record<string, string>) {
-  return Object.entries(styles || {})
+  return Object.entries(styles)
     .map(([key, val]) => `${key}:${val}`)
     .join(';')
 }
